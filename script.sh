1076894a3bac48a8e3e9704cb8ec9d5e1a93a4e7
narayana <jalla.narayanamurthy@gmail.com>
2020-02-26 18:08:58 +0000

    first commit

---
+++ script.sh
@@ -0,0 +1,80 @@
+# generate_package_xml.sh <workspace directory>
+#get list of modified files
+
+
+#export WSPACE=$1
+export WSPACE=/ci/.jenkins/workspace/sfdx_cli_poc
+
+dos2unix $WSPACE/version.txt 2>>/dev/null
+
+#sed -i 's/Package xmlns="http:\/\/soap.sforce.com\/2006\/04\/metadata"/Package/g' $WSPACE/manifest/package.xml
+
+        cd $WSPACE/manifest
+	        echo changing directoy to $WSPACE
+		        cp package.xml{,.bak} &&
+				        echo Backing up package.xml to package.xml.bak
+			read -d '' NEWPKGXML << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<Package>
+</Package>
+EOF
+
+        echo ===PKGXML===
+	        echo $NEWPKGXML
+		        echo Creating new package.xml
+			        echo $NEWPKGXML > $WSPACE/manifest/package.xml
+
+				cd $WSPACE
+				version=$(cat $WSPACE/version.txt)
+				git diff --oneline --name-only $version HEAD | grep -v version.txt |grep -v "Referenced Packages" > $WSPACE/modified_files
+
+				for CFILE in `cat modified_files`
+				do
+					        echo Analyzing file `basename $CFILE`
+
+						        case "$CFILE"
+								                in
+																*.cls*) TYPENAME="ApexClass";;
+																                        *.page*) TYPENAME="ApexPage";;
+																			                        *.component*) TYPENAME="ApexComponent";;
+																						                        *.trigger*) TYPENAME="ApexTrigger";;
+																									                        *.app*) TYPENAME="CustomApplication";;
+																												                        *.labels*) TYPENAME="CustomLabels";;
+																															                        *object*) TYPENAME="CustomObject";;
+																																		                        *tab*) TYPENAME="CustomTab";;
+																																					                        *.resource*) TYPENAME="StaticResource";;
+																																								                        *.workflow*) TYPENAME="Workflow";;
+																																											                        *.remoteSite*) TYPENAME="RemoteSiteSettings";;
+																																														                        *.pagelayout*) TYPENAME="Layout";;
+																																																	                        *) TYPENAME="UNKNOWN TYPE";;
+																																																				        esac
+
+																																																					                if [[ "$TYPENAME" != "UNKNOWN TYPE" ]]
+																																																								                then
+																																																											                        ENTITY=$(basename "$CFILE")
+																																																														                        ENTITY="${ENTITY%.*}"
+																																																																	                        echo ENTITY NAME: $ENTITY
+
+																																																																				                        if grep -Fq "$TYPENAME" $WSPACE/manifest/package.xml
+																																																																								                        then
+																																																																												                                echo Generating new member for $ENTITY
+																																																																																                                xmlstarlet ed -L -s "/Package/types[name='$TYPENAME']" -t elem -n members -v "$ENTITY" $WSPACE/manifest/package.xml
+																																																																																				                        else
+																																																																																								                                echo Generating new $TYPENAME type
+																																																																																												                                xmlstarlet ed -L -s /Package -t elem -n types -v "" $WSPACE/manifest/package.xml
+																																																																																																                                xmlstarlet ed -L -s '/Package/types[not(*)]' -t elem -n name -v "$TYPENAME" $WSPACE/manifest/package.xml
+																																																																																																				                                echo Generating new member for $ENTITY
+																																																																																																								                                xmlstarlet ed -L -s "/Package/types[name='$TYPENAME']" -t elem -n members -v "$ENTITY" $WSPACE/manifest/package.xml
+																																																																																																												                        fi
+																																																																																																															                else
+																																																																																																																		                        echo ERROR: UNKNOWN FILE TYPE $CFILE
+																																																																																																																					                fi
+																																																																																																																						done
+																																																																																																																						echo Cleaning up Package.xml
+																																																																																																																						        xmlstarlet ed -L -s /Package -t elem -n version -v "27.0" $WSPACE/manifest/package.xml
+																																																																																																																							        xmlstarlet ed -L -i /Package -t attr -n xmlns -v "http://soap.sforce.com/2006/04/metadata" $WSPACE/manifest/package.xml
+
+
+																																																																																																																								#echo ====UPDATED PACKAGE.XML====
+																																																																																																																								#sed -i 's/<Package>/<Package xmlns="http:\/\/soap.sforce.com\/2006\/04\/metadata">/g' $WSPACE/manifest/package.xml
+																																																																																																																								cat $WSPACE/manifest/package.xml
